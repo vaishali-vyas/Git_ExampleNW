213b18c1e82464dc2cf504b61c49a35392771af8
Vaishali <vvyas@qa.com>
2021-07-27 23:22:40 +0100

    VV - adding further codes

--- src/main/java/com/natWest/demo/controller/StudentController.java
+++ src/main/java/com/natWest/demo/controller/StudentController.java
@@ -6,10 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.natWest.demo.domain.Student;
 import com.natWest.demo.service.StudentService;
 
@@ -18,28 +22,51 @@ import com.natWest.demo.service.StudentService;
 public class StudentController {
 
 	private StudentService service;
-	
-	
+
 	@Autowired
 	public StudentController(StudentService service) {
 		super();
 		this.service = service;
 	}
 
-	@GetMapping("/test")
-	public String testRequest() {
-		return "Hello";
-	}
-
-	@GetMapping("/get")
-	public Student getStudent() {
-		return new Student("Bob", "Bobson", 19, "bbson@edu.ac.uk");
-
-	}
-
-	@GetMapping("/getAll")
-	public ResponseEntity<List<Student>>readAll() {
+//	@GetMapping("/test")
+//	public String testRequest() {
+//		return "Hello";
+//	}
+//
+//	@GetMapping("/get")
+//	public Student getStudent() {
+//		return new Student("Bob", "Bobson", 19, "bbson@edu.ac.uk");
+//
+//	}
+//Read
+	@GetMapping(path = "readfromdb")
+	public ResponseEntity<List<Student>> getAll() {
 		return new ResponseEntity<List<Student>>(this.service.readAll(), HttpStatus.OK);
 	}
 
+	@GetMapping("/findbyId/{id}")
+	public ResponseEntity<Student> findbyId(@PathVariable("id") Long id) {
+		return new ResponseEntity<Student>(this.service.findbyId(id), HttpStatus.FOUND);
+	}
+//create
+	@PostMapping("/create")
+	public ResponseEntity<Student>create(@RequestBody Student student) {
+		return new ResponseEntity<Student>(this.service.create(student),HttpStatus.CREATED);
+	}
+
+//update
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Student> update(@PathVariable("id")Long id, @RequestBody Student student) {
+		return new ResponseEntity<Student>(this.service.update(id, student),HttpStatus.ACCEPTED);
+	}
+	
+	
+//delete
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Boolean> update(@PathVariable Long id) {
+		
+		return new ResponseEntity<Boolean>(this.service.delete(id),HttpStatus.NO_CONTENT);
+	}
+	
 }
--- src/main/java/com/natWest/demo/service/StudentService.java
+++ src/main/java/com/natWest/demo/service/StudentService.java
@@ -4,31 +4,60 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-
-
 import com.natWest.demo.domain.Student;
+import com.natWest.demo.repo.StudentRepo;
 
 @Service
 public class StudentService {
 
+	public StudentRepo repo;
 	
 	//Read all
+
+//	public List<Student> readAll() {
+//		List<Student> list = Arrays.asList(new Student(1L,"Bob", "Bobson", 19, "bbson@edu.ac.uk"),
+//				new Student(2L,"Steve", "Stevenson", 18, "stevenson@edu.ac.uk"));
+//		return list;
+//
+//	}
+
+	public StudentService(StudentRepo repo) {
+		super();
+		this.repo = repo;
+	}
 	
-
+//read all
 	public List<Student> readAll() {
-		List<Student> list = Arrays.asList(new Student(1L,"Bob", "Bobson", 19, "bbson@edu.ac.uk"),
-				new Student(2L,"Steve", "Stevenson", 18, "stevenson@edu.ac.uk"));
-		return list;
+		return this.repo.findAll();
+	}
+	
+	//Create
+	public Student create(Student student) {
+		return this.repo.saveAndFlush(student);
+	}
+		
+	//Read findbyId
+	public Student findbyId(Long Id) {
+		return this.repo.findById(Id).orElseThrow();
+	}
+	
+	//Update
 
+	public Student update(Long id, Student student) {
+		Student exists = this.repo.getById(id);
+		exists. setFirstName(student.getFirstName());
+		exists. setLastName(student.getLastName());
+		exists. setAge(student.getAge());
+		exists. setEmail(student.getEmail());
+		Student Updated = this.repo.save(exists);		
+		return Updated;
 	}
 
-	
-	
-	
-	
-
-	
-
+	//delete
+	public Boolean delete(Long id) {
+		this.repo.deleteById(id);
+		return this.repo.existsById(id);
+	}
 
 	
 }
